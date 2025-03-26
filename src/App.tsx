2ecf0e8a86066b7db434ab309d8ed12a16abfbaa
2wndrhs <2wndrhs@gmail.com>
2025-03-27 06:29:43 +0900

    build: `antd` 의존성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from 'antd';
+
 function App() {
-  return <div className="font-bold font-pretendard">회원목록</div>;
+  return (
+    <main className="font-pretendard">
+      <Button>회원 목록</Button>
+    </main>
+  );
 }
 
 export default App;
