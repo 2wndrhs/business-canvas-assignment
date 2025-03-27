8a2a48f99d81eec3d530cd605acf3f44da1a94fc
2wndrhs <2wndrhs@gmail.com>
2025-03-28 02:48:57 +0900

    fix: `antd`와 `tailwindcss`의 CSS 우선순위 충돌 문제 수정

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,18 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StyleProvider layer>
-      <ConfigProvider
-        theme={{
-          token: {
-            fontFamily: 'inherit',
-            colorPrimary: '#4A7CFE',
-          },
-        }}
-      >
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: 'inherit',
+          colorPrimary: '#4A7CFE',
+          colorTextPlaceholder: '#00000040',
+        },
+      }}
+    >
+      <StyleProvider layer>
         <App />
-      </ConfigProvider>
-    </StyleProvider>
+      </StyleProvider>
+    </ConfigProvider>
   </StrictMode>,
 );
